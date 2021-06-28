fcf8fbab53fe0ed8e924f3b7f5313864e4f2093b
VictorBortoli <victorferreiradebortoli@gmail.com>
2021-06-28 16:35:37 -0300

    PRova começo

--- src/br/senai/aluno/Aluno.java
+++ src/br/senai/aluno/Aluno.java
@@ -1,5 +1,32 @@
 package br.senai.aluno;
 
-public class Aluno {
+import br.senai.curso.Curso;
+import br.senai.endereco.Endereco;
 
+public class Aluno extends Endereco{
+
+	
+	private String NomeDoAluno;
+	private String IdadeDoAluno;
+	private Curso cursos;
+	public String getNomeDoAluno() {
+		return NomeDoAluno;
+	}
+	public void setNomeDoAluno(String nomeDoAluno) {
+		NomeDoAluno = nomeDoAluno;
+	}
+	public String getIdadeDoAluno() {
+		return IdadeDoAluno;
+	}
+	public void setIdadeDoAluno(String idadeDoAluno) {
+		IdadeDoAluno = idadeDoAluno;
+	}
+	public Curso getCursos() {
+		return cursos;
+	}
+	public void setCursos(Curso cursos) {
+		this.cursos = cursos;
+	}
+	
+	
 }
--- src/br/senai/curso/Curso.java
+++ src/br/senai/curso/Curso.java
@@ -2,4 +2,14 @@ package br.senai.curso;
 
 public class Curso {
 
+	
+	private String NomeCurso;
+
+	public String getCurso() {
+		return NomeCurso;
+	}
+
+	public void setCurso(String curso) {
+		NomeCurso = curso;
+	}
 }
